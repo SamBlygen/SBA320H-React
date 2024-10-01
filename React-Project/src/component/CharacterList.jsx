bedb9a104022097782c1f1b1e3813f57c7a4ae4e
samblygen <samblygen@yahoo.com>
2024-10-01 12:42:35 -0400

    seting up

---
+++ React-Project/src/component/CharacterList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CharacterCard from './CharacterCard'
+
+ const CharacterList =({characters})=>{
+  return(
+    <div className="character-list">
+      {characters.map((character) => (
+        <CharacterCard key={character.name} character={character} />
+      ))}
+    </div>
+  )
+}
+
+export default CharacterList;
